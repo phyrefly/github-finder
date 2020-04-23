a37a44bd3c016f23b22991afe6c7fa19ea4ee241
Adam Schofield <as987@newcastle.edu.au>
2020-04-24 00:16:33 +1000

    Section 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    const name = "John Doe"
-    const loading = false;
-    const showName = true;
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({
+      loading: true,
+    });
 
+    const res = await axios.get("https://api.github.com/users", {
+      auth: {
+        username: `${process.env.REACT_APP_GITHUB_CLIENT_ID}`,
+        password: `${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+      },
+    });
+
+    this.setState({
+      users: res.data,
+      loading: false,
+    });
+  }
+
+  render() {
     return (
       <div className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
